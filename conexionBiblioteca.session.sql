e13606db4712f7cfed1068abd5c69de19e01c5d3
Lau Mendoza <laumendoza@MacBook-Pro-de-Laura.local>
2025-03-13 15:34:10 -0300

    editorial controlador

--- conexionBiblioteca.session.sql
+++ conexionBiblioteca.session.sql
@@ -1,2 +1,16 @@
 
 
+ALTER TABLE editorial MODIFY COLUMN id VARCHAR(36);
+
+
+ALTER TABLE libro DROP FOREIGN KEY FK79q7g2604hcmfdxw6ek3jt4el ;
+ALTER TABLE editorial MODIFY COLUMN id VARCHAR(36);
+ALTER TABLE libro MODIFY COLUMN editorial_id VARCHAR(36);
+ALTER TABLE libro 
+ADD CONSTRAINT FK79q7g2604hcmfdxw6ek3jt4el
+FOREIGN KEY (editorial_id) 
+REFERENCES editorial(id);
+
+show create table editorial;
+show  create table libro;
+select * from editorial;
